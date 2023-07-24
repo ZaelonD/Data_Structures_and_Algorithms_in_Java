03604475b5adb8e609b5d6ba564bb9c4e9277fff
Александр Дубровин <xah799@mail.ru>
2023-07-24 16:44:13 +0300

    Create HighArrayApp

---
+++ src/exercises/chapter_2_arrays/HighArrayApp.java
@@ -0,0 +1,30 @@
+package exercises.chapter_2_arrays;
+
+public class HighArrayApp {
+    public static void main(String[] args) {
+        int maxSize = 100; // array size
+        HighArray arr; // reference to array
+        arr = new HighArray(maxSize); // create the array
+        arr.insert(77); // insert 10 items
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(0);
+        arr.insert(66);
+        arr.insert(33);
+        arr.display(); // display items
+        System.out.println();
+        int searchKey = 35; // search for item
+        if (arr.find(searchKey))
+            System.out.println("Found " + searchKey);
+        else
+            System.out.println("Can’t find " + searchKey);
+        arr.delete(0); // delete 3 items
+        arr.delete(55);
+        arr.delete(99);
+        arr.display(); // display items again
+    }
+}
