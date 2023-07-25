fc5a5404bd307fd304bb9491584cab400ebe0f72
Александр Дубровин <xah799@mail.ru>
2023-07-25 15:04:07 +0300

    Created ArrayBub class, BubbleSortApp class and chapter_3_simple_sorting package

---
+++ src/exercises/chapter_3_simple_sorting/ArrayBub.java
@@ -0,0 +1,38 @@
+package exercises.chapter_3_simple_sorting;
+
+public class ArrayBub {
+    private final long[] a; // ref to array a
+    private int nElems; // number of data items
+
+    // constructor
+    public ArrayBub(int max) {
+        a = new long[max]; // create the array
+        nElems = 0; // no items yet
+    }
+
+    // put element into array
+    public void insert(long value) {
+        a[nElems] = value; // insert it
+        nElems++; // increment size
+    }
+
+    // displays array contents
+    public void display() {
+        for (int j = 0; j < nElems; j++) // for each element,
+            System.out.print(a[j] + " "); // display it
+    }
+
+    public void bubbleSort() {
+        int out, in;
+        for (out = nElems - 1; out > 1; out--) // outer loop (backward)
+            for (in = 0; in < out; in++) // inner loop (forward)
+                if (a[in] > a[in + 1]) // out of order?
+                    swap(in, in + 1); // swap them
+    }
+
+    private void swap(int one, int two) {
+        long temp = a[one];
+        a[one] = a[two];
+        a[two] = temp;
+    }
+}
---
+++ src/exercises/chapter_3_simple_sorting/BubbleSortApp.java
@@ -0,0 +1,23 @@
+package exercises.chapter_3_simple_sorting;
+
+public class BubbleSortApp {
+    public static void main(String[] args) {
+        int maxSize = 100; // array size
+        ArrayBub arr; // reference to array
+        arr = new ArrayBub(maxSize); // create the array
+        arr.insert(77); // insert 10 items
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(0);
+        arr.insert(66);
+        arr.insert(33);
+        arr.display(); // display items
+        System.out.println();
+        arr.bubbleSort(); // bubble sort them
+        arr.display(); // Повторный вывод
+    }
+}
