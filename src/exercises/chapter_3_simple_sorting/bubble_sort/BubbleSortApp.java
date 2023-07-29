151eb47203b80ef58d0e8302a0c2a3a200a26a5c
Александр Дубровин <xah799@mail.ru>
2023-07-29 20:16:16 +0300

    Formatting

--- src/exercises/chapter_3_simple_sorting/bubble_sort/BubbleSortApp.java
+++ src/exercises/chapter_3_simple_sorting/bubble_sort/BubbleSortApp.java
@@ -5,8 +5,7 @@ public class BubbleSortApp {
         int maxSize = 10; // array size
         ArrayBub arr; // reference to array
         arr = new ArrayBub(maxSize); // create the array
-        for (int j = 0; j < maxSize; j++)
-        {
+        for (int j = 0; j < maxSize; j++) {
             long n = (long) (java.lang.Math.random() * (maxSize - 1));
             arr.insert(n);
         }
