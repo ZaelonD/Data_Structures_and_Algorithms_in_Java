2f4936ec73f451e7b2069f5c289aa5747d850255
Александр Дубровин <xah799@mail.ru>
2023-07-29 20:16:41 +0300

    Created array filling with random numbers

--- src/exercises/chapter_3_simple_sorting/insertion_sort/InsertSortApp.java
+++ src/exercises/chapter_3_simple_sorting/insertion_sort/InsertSortApp.java
@@ -2,22 +2,17 @@ package exercises.chapter_3_simple_sorting.insertion_sort;
 
 public class InsertSortApp {
     public static void main(String[] args) {
-        int maxSize = 100; // array size
+        int maxSize = 10; // array size
         ArrayIns arr; // reference to array
         arr = new ArrayIns(maxSize); // create the array
-        arr.insert(77); // insert 10 items
-        arr.insert(99);
-        arr.insert(44);
-        arr.insert(55);
-        arr.insert(22);
-        arr.insert(88);
-        arr.insert(11);
-        arr.insert(0);
-        arr.insert(66);
-        arr.insert(33);
+        for (int j = 0; j < maxSize; j++) {
+            long n = (long) (java.lang.Math.random() * (maxSize - 1));
+            arr.insert(n);
+        }
         arr.display(); // display items
         System.out.println();
         arr.insertionSort(); // insertion-sort them
         arr.display(); // display them again
+        System.out.println("\nMedian: " + arr.median());
     }
 }
