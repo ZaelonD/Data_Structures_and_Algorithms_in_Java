7cf8a7a3c37377112d1158f60e9c9a78b2c7b17c
Александр Дубровин <xah799@mail.ru>
2023-07-29 19:09:51 +0300

    Created array filling with random numbers

--- src/exercises/chapter_3_simple_sorting/bubble_sort/BubbleSortApp.java
+++ src/exercises/chapter_3_simple_sorting/bubble_sort/BubbleSortApp.java
@@ -2,19 +2,14 @@ package exercises.chapter_3_simple_sorting.bubble_sort;
 
 public class BubbleSortApp {
     public static void main(String[] args) {
-        int maxSize = 100; // array size
+        int maxSize = 10; // array size
         ArrayBub arr; // reference to array
         arr = new ArrayBub(maxSize); // create the array
-        arr.insert(77); // insert 10 items
-        arr.insert(99);
-        arr.insert(44);
-        arr.insert(55);
-        arr.insert(22);
-        arr.insert(88);
-        arr.insert(11);
-        arr.insert(0);
-        arr.insert(66);
-        arr.insert(33);
+        for (int j = 0; j < maxSize; j++)
+        {
+            long n = (long) (java.lang.Math.random() * (maxSize - 1));
+            arr.insert(n);
+        }
         arr.display(); // display items
         System.out.println();
         arr.bubbleSort(); // bubble sort them
