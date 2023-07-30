46ff7aaee7401115d04b1e89c4e0a5acd9aa1f57
Александр Дубровин <xah799@mail.ru>
2023-07-30 16:37:27 +0300

    Added testing code for oddEvenSort() method

--- src/exercises/chapter_3_simple_sorting/bubble_sort/BubbleSortApp.java
+++ src/exercises/chapter_3_simple_sorting/bubble_sort/BubbleSortApp.java
@@ -3,15 +3,21 @@ package exercises.chapter_3_simple_sorting.bubble_sort;
 public class BubbleSortApp {
     public static void main(String[] args) {
         int maxSize = 10; // array size
-        ArrayBub arr; // reference to array
+        ArrayBub arr, arr1; // reference to array
         arr = new ArrayBub(maxSize); // create the array
         for (int j = 0; j < maxSize; j++) {
             long n = (long) (java.lang.Math.random() * (maxSize - 1));
             arr.insert(n);
         }
+        arr1 = arr;
         arr.display(); // display items
         System.out.println();
+        arr1.display(); // display items
+        System.out.println();
         arr.bubbleSort(); // bubble sort them
-        arr.display(); // Повторный вывод
+        arr.display(); // display items
+        System.out.println();
+        arr1.oddEvenSort();
+        arr1.display(); // display items
     }
 }
