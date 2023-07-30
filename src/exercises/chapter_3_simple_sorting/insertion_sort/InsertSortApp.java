db21b7e3bd44e24194fc24353318b1f4fc77e12c
Александр Дубровин <xah799@mail.ru>
2023-07-30 14:20:54 +0300

    Created noDups() method

--- src/exercises/chapter_3_simple_sorting/insertion_sort/InsertSortApp.java
+++ src/exercises/chapter_3_simple_sorting/insertion_sort/InsertSortApp.java
@@ -2,7 +2,7 @@ package exercises.chapter_3_simple_sorting.insertion_sort;
 
 public class InsertSortApp {
     public static void main(String[] args) {
-        int maxSize = 10; // array size
+        int maxSize = 25; // array size
         ArrayIns arr; // reference to array
         arr = new ArrayIns(maxSize); // create the array
         for (int j = 0; j < maxSize; j++) {
@@ -13,6 +13,9 @@ public class InsertSortApp {
         System.out.println();
         arr.insertionSort(); // insertion-sort them
         arr.display(); // display them again
+        System.out.println();
+        arr.noDups();
+        arr.display();
         System.out.println("\nMedian: " + arr.median());
     }
 }
