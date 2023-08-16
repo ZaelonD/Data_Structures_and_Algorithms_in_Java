344368ae42a7a2fd90cda27087e07ddf766e2362
Александр Дубровин <xah799@mail.ru>
2023-08-16 14:30:23 +0300

    Created InfixApp

---
+++ src/exercises/chapter_4_stacks_and_queues/infix/StackX.java
@@ -0,0 +1,51 @@
+package exercises.chapter_4_stacks_and_queues.infix;
+
+class StackX {
+    private final int maxSize;
+    private final char[] stackArray;
+    private int top;
+
+    public StackX(int s) {
+        maxSize = s;
+        stackArray = new char[maxSize];
+        top = -1;
+    }
+
+    public void push(char j) {
+        stackArray[++top] = j;
+    }
+
+    public char pop() {
+        return stackArray[top--];
+    }
+
+    public char peek() {
+        return stackArray[top];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public int size() {
+        return top + 1;
+    }
+
+    public char peekN(int n) {
+        return stackArray[n];
+    }
+
+    public void displayStack(String s) {
+        System.out.print(s);
+        System.out.print("Stack (bottom-->top): ");
+        for (int j = 0; j < size(); j++) {
+            System.out.print(peekN(j));
+            System.out.print(' ');
+        }
+        System.out.println();
+    }
+
+    public boolean isFull() {
+        return top == maxSize - 1;
+    }
+}
