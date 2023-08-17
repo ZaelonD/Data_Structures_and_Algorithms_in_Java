44e250b082c865a37f2a1d117ec08e21e263e37b
Александр Дубровин <xah799@mail.ru>
2023-08-17 11:31:34 +0300

    Deleted the comments

--- src/exercises/chapter_4_stacks_and_queues/queue/QueueApp.java
+++ src/exercises/chapter_4_stacks_and_queues/queue/QueueApp.java
@@ -8,19 +8,5 @@ public class QueueApp {
         theQueue.insert(30);
         theQueue.insert(40);
         theQueue.display();
-//        theQueue.remove(); // remove 3 items
-//        theQueue.remove(); // (10, 20, 30)
-//        theQueue.remove();
-//        theQueue.insert(50); // insert 4 more items
-//        theQueue.insert(60); // (wraps around)
-//        theQueue.insert(70);
-//        theQueue.insert(80);
-//        while (!theQueue.isEmpty()) // remove and display
-//        {
-//            long n = theQueue.remove(); // (40, 50, 60, 70, 80)
-//            System.out.print(n);
-//            System.out.print(" ");
-//        }
-//        System.out.println();
     }
 }
