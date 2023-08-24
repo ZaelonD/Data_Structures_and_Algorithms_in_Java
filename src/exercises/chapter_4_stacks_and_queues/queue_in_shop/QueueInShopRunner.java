886c595766bb5826aa528fb581200237a5da4438
Александр Дубровин <xah799@mail.ru>
2023-08-24 13:33:09 +0300

    Created QueueInShopRunner class

---
+++ src/exercises/chapter_4_stacks_and_queues/queue_in_shop/QueueInShopRunner.java
@@ -0,0 +1,8 @@
+package exercises.chapter_4_stacks_and_queues.queue_in_shop;
+
+public class QueueInShopRunner {
+    public static void main(String[] args) {
+        QueueInShopLogic queueInShopLogic = new QueueInShopWithRandom();
+        queueInShopLogic.start();
+    }
+}
