a9c136a504f41be659f23679fc0563eca72f92e9
Александр Дубровин <xah799@mail.ru>
2023-08-25 10:24:05 +0300

    Created Link class

---
+++ src/exercises/chapter_5_linked_lists/link_list/Link.java
@@ -0,0 +1,16 @@
+package exercises.chapter_5_linked_lists.link_list;
+
+public class Link {
+    public int iData;
+    public double dData;
+    public Link next;
+
+    public Link(int iData, double dData) {
+        this.iData = iData;
+        this.dData = dData;
+    }
+
+    public void displayLink() {
+        System.out.println("{" + iData + ", " + dData + "} ");
+    }
+}
