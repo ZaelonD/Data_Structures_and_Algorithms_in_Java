d90b5de503b4336aa15018fa1fa8e15067ca35fe
Александр Дубровин <xah799@mail.ru>
2023-08-25 14:35:06 +0300

    Added delete() method and displayList() method

--- src/exercises/chapter_5_linked_lists/link_list/LinkList.java
+++ src/exercises/chapter_5_linked_lists/link_list/LinkList.java
@@ -27,6 +27,34 @@ public class LinkList {
         return current;
     }
 
+    public Link delete(int key) {
+        Link current = first, previous = first;
+        if (!isEmpty())
+            while (current.iData != key) {
+                if (current.next == null)
+                    return null;
+                else {
+                    previous = current;
+                    current = current.next;
+                }
+            }
+        if (current == first)
+            first = current.next;
+        else
+            previous.next = current.next;
+        return current;
+    }
+
+    public void displayList() {
+        Link current = first;
+        System.out.print("List (first-->last): ");
+        while (current != null) {
+            current.displayLink();
+            current = current.next;
+        }
+        System.out.println();
+    }
+
     public boolean isEmpty() {
         return first == null;
     }
