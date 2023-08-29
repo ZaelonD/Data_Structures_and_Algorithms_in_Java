b371d59f86480d1f2e236e5d6c15c491f64b3f44
Александр Дубровин <xah799@mail.ru>
2023-08-29 23:09:15 +0300

    Moving methods

--- src/exercises/chapter_5_linked_lists/doubly_linked_list/DoublyLinkedList.java
+++ src/exercises/chapter_5_linked_lists/doubly_linked_list/DoublyLinkedList.java
@@ -24,6 +24,75 @@ public class DoublyLinkedList {
             first = last = new Link(data);
     }
 
+    public boolean insertAfter(long key, long data) {
+        if (!isEmpty()) {
+            Link desiredLink = find(key);
+            if (desiredLink == null) {
+                return false;
+            }
+            Link newLink = new Link(data);
+            if (desiredLink == last)
+                last = newLink;
+            else {
+                desiredLink.next.previous = newLink;
+                newLink.next = desiredLink.next;
+            }
+            desiredLink.next = newLink;
+            newLink.previous = desiredLink;
+        }
+        return true;
+    }
+
+    public Link deleteFirst() {
+        Link temp = first;
+        if (!isEmpty()) {
+            if (first.next == null) {
+                first = null;
+                last = null;
+            } else {
+                first.next.previous = null;
+                first = first.next;
+            }
+        } else
+            System.err.println("Can't remove\nList is empty!");
+        return temp;
+    }
+
+    public Link deleteLast() {
+        return null;
+    }
+
+    public Link deleteKey(long key) {
+        Link desiredLink = find(key);
+        if (!isEmpty() && desiredLink != null) {
+            if (desiredLink == first && desiredLink == last) {
+                first = last = null;
+            } else if (desiredLink == first) {
+                desiredLink.next.previous = null;
+                first = desiredLink.next;
+            } else if (desiredLink == last) {
+                desiredLink.previous.next = null;
+                last = desiredLink.previous;
+            } else {
+                desiredLink.previous.next = desiredLink.next;
+                desiredLink.next.previous = desiredLink.previous;
+            }
+        } else
+            System.out.println("Can't remove\nList is empty!");
+        return desiredLink;
+    }
+
+
+    public Link find(long key) {
+        Link current = first;
+        while (current != null && current.data != key) {
+            current = current.next;
+        }
+        if (current == null)
+            System.err.println("Can't find item in list");
+        return current;
+    }
+
     public void displayForward() {
         if (!isEmpty()) {
             System.out.print("List (first-->last): ");
@@ -48,70 +117,6 @@ public class DoublyLinkedList {
         } else
             System.out.println("No items to display\nList is empty!");
         System.out.println();
-    }
-
-    public Link deleteFirst() {
-        Link temp = first;
-        if (!isEmpty()) {
-            if (first.next == null) {
-                first = null;
-                last = null;
-            } else {
-                first.next.previous = null;
-                first = first.next;
-            }
-        } else
-            System.err.println("Can't remove\nList is empty!");
-        return temp;
-    }
-
-    public boolean insertAfter(long key, long data) {
-        if (!isEmpty()) {
-            Link desiredLink = find(key);
-            if (desiredLink == null) {
-                return false;
-            }
-            Link newLink = new Link(data);
-            if (desiredLink == last)
-                last = newLink;
-            else {
-                desiredLink.next.previous = newLink;
-                newLink.next = desiredLink.next;
-            }
-            desiredLink.next = newLink;
-            newLink.previous = desiredLink;
-        }
-        return true;
-    }
-
-    public Link find(long key) {
-        Link current = first;
-        while (current != null && current.data != key) {
-            current = current.next;
-        }
-        if (current == null)
-            System.err.println("Can't find item in list");
-        return current;
-    }
-
-    public Link deleteKey(long key) {
-        Link desiredLink = find(key);
-        if (!isEmpty() && desiredLink != null) {
-            if (desiredLink == first && desiredLink == last) {
-                first = last = null;
-            } else if (desiredLink == first) {
-                desiredLink.next.previous = null;
-                first = desiredLink.next;
-            } else if (desiredLink == last) {
-                desiredLink.previous.next = null;
-                last = desiredLink.previous;
-            } else {
-                desiredLink.previous.next = desiredLink.next;
-                desiredLink.next.previous = desiredLink.previous;
-            }
-        } else
-            System.out.println("Can't remove\nList is empty!");
-        return desiredLink;
     }
 
     public boolean isEmpty() {
