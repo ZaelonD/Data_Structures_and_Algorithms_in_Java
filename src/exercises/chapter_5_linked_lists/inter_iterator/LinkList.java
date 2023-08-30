f95340c8e7bd2b5e6b8ac93ac966ca93ffffff3d
Александр Дубровин <xah799@mail.ru>
2023-08-30 09:37:18 +0300

    Created LinkList class for interIterator program

---
+++ src/exercises/chapter_5_linked_lists/inter_iterator/LinkList.java
@@ -0,0 +1,32 @@
+package exercises.chapter_5_linked_lists.inter_iterator;
+
+import exercises.chapter_5_linked_lists.first_last_list.Link;
+
+public class LinkList {
+    private Link first;
+
+    public Link getFirst() {
+        return first;
+    }
+
+    public void setFirst(Link first) {
+        this.first = first;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void displayList() {
+        if (!isEmpty()) {
+            Link current = first;
+            System.out.print("List (first-->last): ");
+            while (current != null) {
+                current.displayLink();
+                current = current.next;
+            }
+        } else
+            System.err.println("No items to display\nList is empty!");
+        System.out.println();
+    }
+}
