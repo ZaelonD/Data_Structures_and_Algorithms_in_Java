28d1ac31228527ca351a451f12dd95882310cc65
Александр Дубровин <xah799@mail.ru>
2023-08-30 14:39:34 +0300

    Created PriorityQueueApp class

---
+++ src/exercises/chapter_5_linked_lists/sorted_list/PriorityQueueApp.java
@@ -0,0 +1,19 @@
+package exercises.chapter_5_linked_lists.sorted_list;
+
+import exercises.chapter_5_linked_lists.priority_queue_on_sorted_list_implementation.PriorityQueue;
+
+public class PriorityQueueApp {
+    public static void main(String[] args) {
+        PriorityQueue priorityQueue = new PriorityQueue();
+        priorityQueue.insert(20);
+        priorityQueue.insert(80);
+        priorityQueue.insert(40);
+        priorityQueue.remove();
+        priorityQueue.insert(30);
+        priorityQueue.insert(10);
+        while (!priorityQueue.isEmpty()) {
+            System.out.print(priorityQueue.peek() + "\t");
+            priorityQueue.remove();
+        }
+    }
+}
